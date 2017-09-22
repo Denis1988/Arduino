cd3417f1a04062545dfd3cc38c5e9f8e03e5c5ae
Denis1988 <denis1988@gmail.com>
2017-09-22 22:57:52 +0300

    Create oledJoysticRF.ino

---
+++ oledJoysticRF.ino
@@ -0,0 +1,112 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+#include <Adafruit_SSD1306.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#define OLED_RESET 4
+RF24 radio(7, 8); // CNS, CE
+Adafruit_SSD1306 display(OLED_RESET);
+
+
+const uint64_t pipe = 1;
+
+struct JOY {
+  int x1;
+  int x2;
+  int y1;
+  int y2;
+  boolean s1;
+  boolean s2;
+};
+
+
+#if (SSD1306_LCDHEIGHT != 32)
+#error("Height incorrect, please fix Adafruit_SSD1306.h!");
+#endif
+
+
+ int joyPinX1 = 14;                 // slider variable connecetd to analog pin 0
+ int joyPinY1 = 15;                 // slider variable connecetd to analog pin 1
+ int joyPinX2 = 16;
+ int joyPinY2 = 17;
+ int valueX1 = 0;                  // variable to read the value from the analog pin 0
+ int valueX2 = 0;                  // variable to read the value from the analog pin 1
+ int valueY1 = 0;
+ int valueY2 = 0;
+ int joySW1 = 5;
+ int joySW2 = 6;
+ int valueSW1 = 0;
+ int valueSW2 = 0;
+ int one = 0,two = 0;
+ int LED = A6;
+
+void setup() {
+  
+  //Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(pipe);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.setDataRate(RF24_250KBPS);
+  radio.setChannel(110);
+  radio.stopListening();
+
+  
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  pinMode(joySW1, INPUT_PULLUP);
+  pinMode(joySW2, INPUT_PULLUP);
+  pinMode(LED,OUTPUT);
+
+}
+
+void loop() {
+  valueX1 = analogRead(joyPinX1);  
+  valueY1 = analogRead(joyPinY1);  
+  valueX2 = analogRead(joyPinX2);
+  valueY2 = analogRead(joyPinY2);
+  valueSW1 = digitalRead(joySW1);
+  valueSW2 = digitalRead(joySW2);
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.print("X1 : ");
+  display.print(valueX1);
+  display.setCursor(65,0);
+  display.print("Y1 : ");
+  display.println(valueY1);
+  display.print("X2 : ");
+  display.print(valueX2);
+  display.setCursor(65,8);
+  display.print("Y2 : ");
+  display.println(valueY2);
+  display.print("J1: ");
+  if(valueSW1 == LOW){
+  display.print("True");
+  one = 1;
+  }
+  else{
+  display.print("False");
+  one = 0;
+  }
+  display.setCursor(65,17);
+  display.print("J2: ");
+  if(valueSW2 == LOW){
+  display.print("True");
+  two = 1;
+  }
+  else{
+  display.print("False");
+  two = 0;
+  }
+  if(valueSW2 == HIGH && valueSW1 == HIGH){
+    digitalWrite(LED,HIGH);
+  }
+  display.display();
+
+  JOY joy = {valueX1,valueX2,valueY1,valueY2,(boolean)valueSW1,(boolean)valueSW2};
+  radio.write(&joy, sizeof(joy));
+  
+  delay(100);
+}
